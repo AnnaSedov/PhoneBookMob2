9eefd2362e1c8001b93c5d3f8b09e1c5ef0525e0
AnnaSedov <155672179+AnnaSedov@users.noreply.github.com>
2024-04-28 00:15:32 +0300

    280424
    hw add contact

--- src/main/java/screens/ContactListScreen.java
+++ src/main/java/screens/ContactListScreen.java
@@ -45,34 +45,45 @@ public class ContactListScreen extends BaseScreen{
 
     public boolean isContactAdded(Contact contact) {
 
-        List<MobileElement> containers=driver.findElements(By.xpath("//*[@resource-id='com.sheygam.contactapp:id/rowContainer']"));
+List<MobileElement> containers=driver.findElementsByClassName("android.widget.TextView");//android.widget.TextView
+        if(containers.size()>0) {
+            for (MobileElement container : containers) {
+                System.out.println(container.getText()+"***"+contact.getName()+" "+contact.getLastName());
+                if(container.getText().equals(contact.getName()+" "+contact.getLastName())){
+                    return true;
+                }
+            }
+        }
 
-        if(containers.size()>0){
-for(MobileElement container:containers){
 
 
-       waitForAnElement(container);
-        container.click();
-        MobileElement elementName=driver.findElement(By.xpath("//*[@resource-id='com.sheygam.contactapp:id/nameTxt']"));
-        String elementNameVal=elementName.getAttribute("value");
-          MobileElement elementLastName=driver.findElement(By.xpath("//[*@resource-id='com.sheygam.contactapp:id/lastNameTxt']"));
-          String elementLastNameVal=elementLastName.getAttribute("value");
-          MobileElement elementPhone=driver.findElement(By.xpath("//*[@resource-id='com.sheygam.contactapp:id/phoneTxt']"));
-          String elementPhoneVal=elementPhone.getAttribute("value");
-          MobileElement elementAddress=driver.findElement(By.xpath("//*[@resource-id='com.sheygam.contactapp:id/addressTxt']"));
-          String elementAddressVal=elementName.getAttribute("value");
-          MobileElement elementEmail=driver.findElement(By.xpath("//*[@resource-id='com.sheygam.contactapp:id/emailTxt']"));
-          String elementEmailVal=elementName.getAttribute("value");
-          MobileElement elementDescription=driver.findElement(By.xpath("//*[@resource-id='com.sheygam.contactapp:id/descTxt']"));
-          String elementDescriptionVal=elementName.getAttribute("value");
-          Contact listContact= new Contact();
-          listContact.setName(elementNameVal);
-          listContact.setLastName(elementLastNameVal);
-          listContact.setAddress(elementAddressVal);
-          listContact.setPhone(elementPhoneVal);
-          listContact.setDescriptions(elementDescriptionVal);
-          listContact.setEmail(elementEmailVal);
-          return listContact.equals(contact);}}
+        //  List<MobileElement> containers=driver.findElements(By.xpath("//*[@resource-id='com.sheygam.contactapp:id/rowContainer']"));
+//        if(containers.size()>0){
+//for(MobileElement container:containers){
+//
+//
+//       waitForAnElement(container);
+//        container.click();
+//        MobileElement elementName=driver.findElement(By.xpath("//*[@resource-id='com.sheygam.contactapp:id/nameTxt']"));
+//        String elementNameVal=elementName.getAttribute("value");
+//          MobileElement elementLastName=driver.findElement(By.xpath("//[*@resource-id='com.sheygam.contactapp:id/lastNameTxt']"));
+//          String elementLastNameVal=elementLastName.getAttribute("value");
+//          MobileElement elementPhone=driver.findElement(By.xpath("//*[@resource-id='com.sheygam.contactapp:id/phoneTxt']"));
+//          String elementPhoneVal=elementPhone.getAttribute("value");
+//          MobileElement elementAddress=driver.findElement(By.xpath("//*[@resource-id='com.sheygam.contactapp:id/addressTxt']"));
+//          String elementAddressVal=elementName.getAttribute("value");
+//          MobileElement elementEmail=driver.findElement(By.xpath("//*[@resource-id='com.sheygam.contactapp:id/emailTxt']"));
+//          String elementEmailVal=elementName.getAttribute("value");
+//          MobileElement elementDescription=driver.findElement(By.xpath("//*[@resource-id='com.sheygam.contactapp:id/descTxt']"));
+//          String elementDescriptionVal=elementName.getAttribute("value");
+//          Contact listContact= new Contact();
+//          listContact.setName(elementNameVal);
+//          listContact.setLastName(elementLastNameVal);
+//          listContact.setAddress(elementAddressVal);
+//          listContact.setPhone(elementPhoneVal);
+//          listContact.setDescriptions(elementDescriptionVal);
+//          listContact.setEmail(elementEmailVal);
+//          return listContact.equals(contact);}}
 
         return false;
 
